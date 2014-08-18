6e3f9c381301f41f7e715eac11fcb2694a1c4f16
Paris Xavier Pinkney <paris@pxpgraphics.com>
2014-08-18 15:28:16 -0700

    Added project.

---
+++ MQPasscodeControllerSample/MQPasscodeControllerSample/MQAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MQAppDelegate.h
+//  MQPasscodeControllerSample
+//
+//  Created by Paris Pinkney on 8/18/14.
+//  Copyright (c) 2014 Marqeta, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MQAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
